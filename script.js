396bfd29288af3b31c2dd63ab84bcaf21bcf4df4
Kailas54 <kidg248@gmail.com>
2025-02-22 20:26:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav');
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+    
+
+}
