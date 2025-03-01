bb2efa54105ece5db047a88a69ca8bd588315be9
Kailas54 <kidg248@gmail.com>
2025-03-01 16:23:50 +0530

    second commit

---
+++ email-config.js
@@ -0,0 +1,6 @@
+// Replace these with your actual credentials
+const SMTP_CONFIG = {
+    SecureToken: " c88e2e03-d0c0-4a78-954d-93b2c07ff8e2",
+    FromEmail: "kidg248@gmail.com",
+    PDFPath: "./Mclaren-brochure.pdf"
+};
--- script.js
+++ script.js
@@ -16,3 +16,35 @@ document.querySelectorAll(".trailer").forEach(video => {
         this.currentTime = 0; // Reset video to start when unhovered
     });
 });
+
+function changeVideo(name) {
+    const bgVideoList = document.querySelectorAll('.bg-video');
+    const trailers = document.querySelectorAll('.screen-video');
+    const models = document.querySelectorAll('.model');
+
+    const carColors = {
+        'maclaren-1': '#08de49',    
+        'maclaren-2': '#4287f5',    
+        'maclaren-3': '#ff0000',   
+        'maclaren-4': '#ff8c00',    
+        'maclaren-5': '#808080', 
+    };
+
+    
+    if (carColors[name]) {
+        document.documentElement.style.setProperty('--primary-color', carColors[name]);
+    }
+    bgVideoList.forEach(video => {
+        video.classList.remove('active');
+        if (video.classList.contains(name)) {
+            video.classList.add('active');
+            }
+    }); 
+
+    models.forEach(model => {
+        model.classList.remove('active');
+        if (model.classList.contains(name)) {
+            model.classList.add('active');
+            }
+    }); 
+}
